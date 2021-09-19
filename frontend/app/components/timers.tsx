635dcdbd37cc2bbbc85771e2f97fbd11f8906021
Jan Lindauer <lindajan@fel.cvut.cz>
2021-09-19 22:03:01 +0200

    frontend: Improve timers

--- frontend/app/components/timers.tsx
+++ frontend/app/components/timers.tsx
@@ -1,64 +1,64 @@
 "use strict";
 
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+import { isDefined } from '../helpers/common';
 
 
-const calculateElapsedTime = (start) => {
-	let difference = new Date() - start;
-	difference = (difference>5*60*1000) ? 5*60*1000 : difference;
-    const timeLeft = {
-      	minutes: Math.floor((difference / 1000 / 60) % 60),
-      	seconds: Math.floor((difference / 1000) % 60),
-      	milliseconds: Math.floor((difference / 10) % 100)
-    };
-}
+export const TimeDisplay = ({ name, time, className }) => (
+	<div className={classNames('timer', className)}>
+		<span className="timerName">
+			{name}
+		</span>
+		<span className="digits">
+        	{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+      	</span>
+		<span className="digits">
+        	{("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
+      	</span>
+		<span className="digits">
+        	{("0" + Math.floor((time / 10) % 100)).slice(-2)}
+      	</span>
+	</div>
+);
 
+export const Timer = ({ name, start, active, className }) => {
 
-//name, start, active
-export function Timer(props) {
-	const [time, setTime] = useState(new Date() - props.start);
+	const [currentTime, setCurrentTime] = useState(() => Date.now());
 
+	// TODO: more efficient solution (perhaps update the HTML DOM directly?)
 	useEffect(() => {
-		if (props.active){
-  			const timer = setTimeout(() => {
-    			setTime(new Date() - props.start);
- 			}, 130);
- 		}
-	});
+
+		let interval: number | undefined;
+
+		if (active) {
+			interval = window.setInterval(() => {
+				setCurrentTime(Date.now());
+			}, 100);
+		}
+
+		return () => {
+
+			if (isDefined(interval)) {
+				window.clearInterval(interval);
+			}
+
+		};
+
+	}, [active, setCurrentTime]);
 
 	return (
-		<TimeDisplay name={props.name} time={time} />
+		<TimeDisplay className={className} name={name} time={currentTime - start} />
 	);
-}
+};
 
-export function TimeDisplay(props) {
-	return (
-		<div className="timer">
-			<span className="timer_name">
-				{props.name}
-			</span>
-      		<span className="digits">
-        		{("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
-      		</span>
-      		<span className="digits">
-        		{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
-      		</span>
-      		<span className="digits">
-        		{("0" + Math.floor((props.time / 10) % 100)).slice(-2)}
-      		</span>
-    	</div>
-	);
-}
-
-export function Timers(props) {
-	return (
-		<div>
-			<Timer name="Total time: " start={props.raceStartTime} active={props.active}/>
-			<Timer name="Lap time: " start={props.lapStartTime} active={props.active}/>
-			<TimeDisplay name="Best lap: " time={props.bestLapTime}/>
-		</div>
-	);
-}
+export const Timers = props => (
+	<div className="timers">
+		<Timer className="total" name="Total time: " start={props.raceStartTime} active={props.active} />
+		<Timer className="this" name="Lap time: " start={props.lapStartTime} active={props.active} />
+		<TimeDisplay className="best" name="Best lap: " time={props.bestLapTime} />
+	</div>
+);
 
 
 
