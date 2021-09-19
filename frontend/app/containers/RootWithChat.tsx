42cea1301c14b12d27e92a71fb7ff62206cc0939
Jan Lindauer <lindajan@fel.cvut.cz>
2021-09-19 18:14:47 +0200

    frontend: Implement a simple WebSocket-based chat React.js demo with React Hooks (for learning purposes)

---
+++ frontend/app/containers/RootWithChat.tsx
@@ -0,0 +1,146 @@
+"use strict";
+
+// This is an example of implementing WebSocket-based chat app in React.js using Hooks
+// TODO: This was implemented only fur learning purposes. Will remove in the future.
+
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import Store from '../store/Store';
+import Router from '../router/Router';
+
+import { AppState } from '../types';
+
+const ConfigContext = React.createContext<string>('localhost');
+
+export interface RootProps {
+	store: Store<AppState>;
+	router: Router;
+}
+
+const useChat = () => {
+
+	const clientRef = useRef<WebSocket | undefined>(undefined);
+
+	const [messages, setMessages] = useState<string[]>([]);
+
+	const addMessage = useCallback(
+		(message: string) => setMessages(prev => [...prev, message]),
+		[setMessages],
+	);
+
+	const sendMessage = useCallback(
+		(message: string) => {
+			if (clientRef.current?.readyState === WebSocket.OPEN) {
+				console.log('sending message', message);
+				clientRef.current?.send(message);
+			} else {
+				console.error('could not send message', clientRef.current);
+			}
+		},
+		[],
+	);
+
+	useEffect(() => {
+
+		console.log('subscribing ...');
+
+		const client = new WebSocket('ws://localhost:4000');
+
+		client.onopen = (event) => {
+			console.log('ws: open', event);
+		};
+
+		client.onclose = (event) => {
+			console.log('ws: close', event);
+		};
+
+		client.onmessage = (event) => {
+			console.log('ws: message', event.data);
+			addMessage(event.data);
+		};
+
+		client.onerror = (event) => {
+			console.error('ws: error', event);
+		};
+
+		clientRef.current = client;
+
+		return () => {
+
+			console.log('unsubscribing (closing client)...');
+
+			// 1000 means "The connection successfully completed the purpose for which it was created."
+			client.close(1000);
+			clientRef.current = undefined;
+
+		};
+
+	}, [addMessage]);
+
+	return {
+		messages,
+		addMessage,
+		sendMessage,
+	};
+
+};
+
+const MyChat = (props) => {
+
+	// const { url } = props;
+
+	const url = useContext(ConfigContext);
+
+	const [counter, setCounter] = useState(0);
+
+	const { messages, addMessage, sendMessage } = useChat();
+
+
+	const handleClick = event => {
+		setCounter(pv => pv + 1);
+		sendMessage(`counter is ${counter}`);
+	};
+
+	return (
+		<div>
+			chat
+			{counter}
+			<button onClick={handleClick}>Count!</button>
+			<ul>
+				{messages.map((message, index) => {
+					return <li key={index}>{message}</li>;
+				})}
+			</ul>
+		</div>
+	);
+
+};
+
+
+const Root = ({ store, router }: RootProps) => {
+
+	const [counter, setCounter] = useState(0);
+	const [chatActive, setChatActive] = useState(true);
+
+	const handleToggleChatBest = useCallback(event => {
+		setChatActive(prevState => !prevState);
+	}, [setChatActive]);
+
+	const handleClick = useCallback(event => {
+		setCounter(prevState => prevState + 1);
+	}, [setCounter]);
+
+	return (
+		<div>
+			<ConfigContext.Provider value="test">
+				my best app: {counter}<br />
+				<button onClick={handleClick}>Root count!</button>
+				<button onClick={handleToggleChatBest}>Toggle chat</button>
+				{chatActive && <MyChat />}
+			</ConfigContext.Provider>
+		</div>
+	);
+
+};
+
+export default Root;
+
