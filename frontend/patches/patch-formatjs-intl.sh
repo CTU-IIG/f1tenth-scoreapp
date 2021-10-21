546d2722463524f54b08d78191063f65fd6114ce
Martin Endler <endlemar@fel.cvut.cz>
2021-10-22 00:57:36 +0200

    frontend: Upgrade dependencies

--- frontend/patches/patch-formatjs-intl.sh
+++ frontend/patches/patch-formatjs-intl.sh
@@ -6,16 +6,16 @@ set -e
 #   fractionalSecondDigits is missing in dateTime.ts DATE_TIME_FORMAT_OPTIONS
 #   see https://github.com/formatjs/formatjs/blob/main/packages/intl/src/dateTime.ts#L7
 
-# This is temporary patch script working only for @formatjs/intl version 1.14.1
+# This is temporary patch script working only for @formatjs/intl version 1.14.3
 # This script must be run from the frontend root
 
-if ! grep '"version": "1.14.1"' "node_modules/@formatjs/intl/package.json" 1>/dev/null; then
-	echo "@formatjs/intl version 1.14.1 not found (node_modules/@formatjs/intl/package.json)"
+if ! grep '"version": "1.14.3"' "node_modules/@formatjs/intl/package.json" 1>/dev/null; then
+	echo "@formatjs/intl version 1.14.3 not found (node_modules/@formatjs/intl/package.json)"
 	exit 1
 fi
 
 if grep "fractionalSecondDigits" "node_modules/@formatjs/intl/lib/src/dateTime.js" 1>/dev/null && grep "fractionalSecondDigits" "node_modules/@formatjs/intl/src/dateTime.js" 1>/dev/null; then
-	echo "skipping @formatjs/intl version 1.14.1 patching (already patched)"
+	echo "skipping @formatjs/intl version 1.14.3 patching (already patched)"
 	exit 0
 fi
 
